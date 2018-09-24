9cc082a5833ef6ff263b2e924542fb7869cadeac
simpart <simpart>
2018-09-24 18:19:42 +0000

    first

---
+++ src/hdr/com.h
@@ -0,0 +1,27 @@
+/**
+ * @file com.h
+ * @brief defined common
+ * @author simpart
+ */
+
+#ifndef __COM_H__
+#define __COM_H__
+
+/*** define ***/
+/**
+ * @brief value of function return
+ */
+#define COM_OK  0  //! function is successful
+#define COM_NG -1  //! function is failed
+
+/**
+ * @brief flag value
+ */
+#define COM_TRUE  1
+#define COM_FALSE 0
+
+
+/*** prototype ***/
+
+#endif
+/* end of file */
